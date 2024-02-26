8689d45d30add5735563f2ccccd426398f16b5a8
Paulson Jose <paul@hackermail.com>
2024-02-26 20:22:28 +0530

    Final commmit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const tile = document.querySelectorAll('.tile')
+const table = document.querySelectorAll('table')
+const circle = document.querySelectorAll('.circle')
+const innerCircle = document.querySelectorAll('.innerCircle')
+const total = document.querySelector('.total')
+const price = document.querySelectorAll('.price')
+
+
+tile.forEach((t,id)=>{
+    t.addEventListener('click', e=>{
+        table.forEach(tb=>tb.classList.remove('active'))
+        circle.forEach(cir=>cir.classList.remove('activeCircle'))
+        innerCircle.forEach(inn=>inn.classList.remove('show'))
+        table[id].classList.add('active')
+        circle[id].classList.add('activeCircle')
+        innerCircle[id].classList.add('show')
+        total.innerText = price[id].innerText
+    })
+})
+
